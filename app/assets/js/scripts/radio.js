2cb5819a222ec601dce1edb6e2d3b104f3321022
pioupia <pioupiadeux@gmail.com>
2023-05-13 19:26:52 +0200

    Fix: background button color

--- app/assets/js/scripts/radio.js
+++ app/assets/js/scripts/radio.js
@@ -5,6 +5,10 @@ let interval;
 const radio = new Audio('http://radio.zone-delta.xyz/listen/zone_delta/radio.mp3');
 
 const homeRadioButton = document.getElementsByClassName('radio-home-button')?.[0];
+
+/**
+ * @type {HTMLImageElement | undefined}
+ */
 const radioImage = document.getElementsByClassName('musique-img')?.[0];
 const radioTitle = document.getElementsByClassName('musique-title')?.[0];
 const authorName = document.getElementsByClassName('author-title')?.[0];
@@ -84,4 +88,64 @@ async function loadNowPlaying() {
     }
 
     radioTitle.textContent = data?.now_playing?.song?.title;
+}
+
+radioImage.onload = () => {
+    const { r, g, b } = getColorAverage();
+    if (r*0.299 + g*0.587 + b*0.114 > 120) {
+        document.documentElement.style.setProperty('--button-bg-color', '#000');
+        document.documentElement.style.setProperty('--button-fg-color', '#fff');
+    } else {
+        document.documentElement.style.setProperty('--button-bg-color', '#fff');
+        document.documentElement.style.setProperty('--button-fg-color', '#000');
+    }
+};
+
+
+/**
+ * Author: pioupia (https://github.com/pioupia)
+ * Made the 13th of May 2023
+ * Licence: creative common. Need to be credited
+ */
+
+/**
+ * Get the color average under the "home" button
+ * @return {{r: number, g: number, b: number}}
+ */
+function getColorAverage() {
+    const canvas = document.createElement('canvas');
+    canvas.height = radioImage.naturalHeight || radioImage.offsetHeight || radioImage.height;
+    canvas.height = radioImage.naturalWidth || radioImage.offsetWidth || radioImage.width;
+
+    const ctx = canvas.getContext('2d');
+
+    const defaultLayers = {
+        r: 0,
+        g: 0,
+        b: 0
+    };
+
+    ctx.drawImage(radioImage, 0, 0);
+
+    try {
+        const midWidth = Math.floor(canvas.width / 4);
+        const midHeight = Math.floor(canvas.height / 2);
+
+        const data = ctx.getImageData(midWidth, midHeight, canvas.width - midWidth, canvas.height - Math.floor(midHeight / 2));
+        const len = data.data.length;
+
+        for (let i = 0; i < len; i += 4) {
+            defaultLayers.r += data.data[i];
+            defaultLayers.g += data.data[i+1];
+            defaultLayers.b += data.data[i+2];
+        }
+
+        const pixelCount = Math.floor(len / 4);
+        defaultLayers.r /= pixelCount;
+        defaultLayers.g /= pixelCount;
+        defaultLayers.b /= pixelCount;
+
+    } catch(_) { /* empty */ }
+
+    return defaultLayers;
 }
