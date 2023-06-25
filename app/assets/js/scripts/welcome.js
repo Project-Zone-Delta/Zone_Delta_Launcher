6f5016f52f84777a86055ee4e20a82dd72d03360
Romain <51379301+thetimedevs@users.noreply.github.com>
2023-06-25 23:08:13 +0200

    LoginOption - Update welcome.js
    Suppression de la fonction LoginOption

--- app/assets/js/scripts/welcome.js
+++ app/assets/js/scripts/welcome.js
@@ -2,8 +2,6 @@
  * Script for welcome.ejs
  */
 document.getElementById('welcomeButton').addEventListener('click', e => {
-    loginOptionsCancelEnabled(false) // False by default, be explicit.
-
     loginOptionsViewOnLoginSuccess = VIEWS.landing
     loginOptionsViewOnLoginCancel = VIEWS.welcome
     switchView(VIEWS.welcome, VIEWS.login, () => {
